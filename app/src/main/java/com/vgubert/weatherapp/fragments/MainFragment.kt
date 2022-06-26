fd67ce84db813eb087de58bc22f529c4f801cc06
Vyacheslav <v.guber2000@gmail.com>
2022-06-26 20:59:27 +0300

    ViewPager Adapter и TabLayout.

--- app/src/main/java/com/vgubert/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/vgubert/weatherapp/fragments/MainFragment.kt
@@ -9,10 +9,21 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.vgubert.weatherapp.R
+import com.vgubert.weatherapp.adapters.VpAdapter
 import com.vgubert.weatherapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var plauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -27,6 +38,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp) {
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener() {
